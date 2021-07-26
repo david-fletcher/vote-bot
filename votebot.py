f6c7b141ced0c6f4aa33689447d9fe2db6b93b7e
David Fletcher <david.login@aol.com>
2021-07-26 11:03:16 -0400

    updates to votebot to make it better

--- votebot.py
+++ votebot.py
@@ -18,6 +18,7 @@ bot_configure_channel = 'bot-stuff'
 bot_help_string = ('VoteBot Help:\n'
                    f'**!vb watch <channel mention>**: Start adding {up_emoji} and {down_emoji} to every message\n'
                    f'**!vb unwatch <channel mention>**: Stop adding {up_emoji} and {down_emoji} to every message\n'
+                   '**!vb show**: show a list of watched channels in this server\n'
                    '**!vb help**: view this menu')
 
 @client.event
@@ -33,15 +34,24 @@ async def on_message(message):
             if (cmd_words[1] == 'watch'):
                 if (len(message.channel_mentions) > 1):
                     await message.channel.send('Please only configure one channel at a time.')
+                    return
                 if (len(message.channel_mentions) < 1):
-                    await message.channel.send('You must link the channel to watch.')
+                    await message.channel.send('You must link a valid channel to watch.')
+                    return
 
                 channel = message.channel_mentions[0]
                 if (not channel):
                     await message.channel.send(f'Channel does not exist.')
                 else:
-                    watch_list.append(channel.id)
-                    await message.channel.send(f'Added {channel.mention} to the watch list.')
+                    if (channel.id in watch_list): 
+                        await message.channel.send(f'{channel.mention} is already in the watch list.')
+                        return
+
+                    if (message.guild.id == channel.guild.id):
+                        watch_list.append(channel.id)
+                        await message.channel.send(f'Added {channel.mention} to the watch list.')
+                    else:
+                        await message.channel.send('You cannot add a channel from another server to the watch list!')
                 return
 
             if (cmd_words[1] == 'unwatch'):
@@ -49,22 +59,44 @@ async def on_message(message):
                     await message.channel.send('Please only configure one channel at a time.')
                     return
                 if (len(message.channel_mentions) < 1):
-                    await message.channel.send('You must link the channel to unwatch.')
+                    await message.channel.send('You must link a valid channel to unwatch.')
                     return
 
                 channel = message.channel_mentions[0]
                 if (not channel):
                     await message.channel.send(f'Channel does not exist.')
                 else:
-                    watch_list.remove(channel.id)
-                    await message.channel.send(f'Removed {channel.mention} from the watch list.')
+                    if (channel.id not in watch_list):
+                        await message.channel.send(f'{channel.mention} was not in the watch list.')
+                        return
+                        
+                    if (message.guild.id == channel.guild.id):
+                        watch_list.remove(channel.id)
+                        await message.channel.send(f'Removed {channel.mention} from the watch list.')
+                    else:
+                        await message.channel.send('You cannot unwatch a channel from a different server!')
+                return
+
+            if (cmd_words[1] == 'show'):
+                ch_list = list(filter(lambda ch: ch.id in watch_list, message.guild.channels))
+
+                if (len(ch_list) < 1): 
+                    await message.channel.send('I am not currently watching any channels from this server.')
+                    return
+
+                ch_name_list = '\n'.join(list(map(lambda ch: ch.mention, ch_list)))
+                ch_list_msg = ('I am currently watching the following from this server:\n'
+                               f'{ch_name_list}'
+                              )
+
+                await message.channel.send(ch_list_msg)
                 return
             
             if (cmd_words[1] == 'help'):
                 await message.channel.send(bot_help_string)
                 return
 
-            await message.channel.send(f'I do not know how to {cmd_words[1]}')
+            await message.channel.send(f'I do not recognize this command: {cmd_words[1]}')
             return
 
         if ((cmd_words[0] == '!vb') and (len(cmd_words) <= 1)):
