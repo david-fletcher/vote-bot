1d2c89630e431aa0b2b4d2d489376c53f90a8930
David Fletcher <david.login@aol.com>
2021-07-29 12:34:11 -0400

    add exclusive configuration feature

--- votebot.py
+++ votebot.py
@@ -146,6 +146,34 @@ async def vote_on_mention(ctx, yesno):
     else: 
         await ctx.send('This command only accepts the words "yes" or "no".')
 
+
+@bot.command(
+    help="Add or update the channel used to configure the bot. If set, the bot will only respond to configuration changes within the specified channel.",
+    brief="Update bot to only be configurable in given channel.",
+    name="upd-conf-channel"
+)
+async def update_config_channel(ctx, channel: discord.TextChannel):
+    global server_data
+    # update settings and save
+    server_data[str(ctx.guild.id)]['config_channel'] = channel.id
+    await ctx.send(f'Updated bot configuration channel to {channel.mention}')
+
+    write_config_to_file()
+
+
+@bot.command(
+    help="Clear the configuration channel option, allowing the bot to be configured in any channel.",
+    brief="Clear exclusive bot configuration channel.",
+    name="clr-conf-channel"
+)
+async def remove_config_channel(ctx):
+    global server_data
+    # update settings and save
+    server_data[str(ctx.guild.id)]['config_channel'] = None
+    await ctx.send('Removed bot configuration channel. Bot is now configurable anywhere in the server.')
+
+    write_config_to_file()
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
